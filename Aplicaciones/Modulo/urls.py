1ef4937433f2f560225bf72dc04be2dba30c28cd
Guidzero <guidopr27@hotmail.com>
2023-07-02 14:25:08 -0600

    Agregando urls

--- Aplicaciones/Modulo/urls.py
+++ Aplicaciones/Modulo/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import booksPage, bookDetail, bookRequest, adminView, adminBookDetail, adminDeleteBook, adminAddBook, adminRequestList, LogInUser
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import logout_then_login
 
 app_name='modulo'
 urlpatterns = [
-    path('', LogInUser, name='login'),
-    path('BooksPage', booksPage, name='BooksPage'),
+    path('', login_required(booksPage), name='BooksPage'),
+    path('accounts/login/', LogInUser, name = 'login'),
+    path('Logout/', logout_then_login, name='logout'),
     path('BookDetail/<id>',bookDetail, name='BookDetail'),
     path('BookRequest/<id>', bookRequest, name='BookRequest'),
     path('AdminBooksPage/', login_required(adminView), name='AdminBooksPage'),
