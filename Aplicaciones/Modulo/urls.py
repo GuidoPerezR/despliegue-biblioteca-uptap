e7794ff147bb1fadd532a558f97620f157f2ee23
Guidzero <guidopr27@hotmail.com>
2023-07-07 07:16:21 -0600

    Viendo que pei

--- Aplicaciones/Modulo/urls.py
+++ Aplicaciones/Modulo/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 from .views import booksPage, bookDetail, bookRequest, adminView, adminBookDetail, adminDeleteBook, adminAddBook, adminRequestList, LogInUser
+from . import views
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import logout_then_login
 
 app_name='modulo'
 urlpatterns = [
-    path('', login_required(booksPage), name='BooksPage'),
+    path('', views.LogInUser, name='login'),
+    """ path('', login_required(booksPage), name='BooksPage'),
     path('BookDetail/<id>',bookDetail, name='BookDetail'),
     path('BookRequest/<id>', bookRequest, name='BookRequest'),
     path('AdminBooksPage/', adminView, name='AdminBooksPage'),
     path('AdminBookDetail/<id>/', adminBookDetail, name='AdminBookDetail'),
     path('AdminDeleteBook/<id>/',adminDeleteBook, name='AdminDeleteBook'),
     path('AdminAddBook/', adminAddBook, name='AdminAddBook'),
-    path('AdminRequestList/', adminRequestList, name='AdminRequestList'),
+    path('AdminRequestList/', adminRequestList, name='AdminRequestList'), """
 ] 
