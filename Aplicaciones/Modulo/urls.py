abd995c5f9ce508b27c84356800c2bee4ff714fe
Guidzero <guidopr27@hotmail.com>
2023-07-07 07:20:01 -0600

    Comentando

--- Aplicaciones/Modulo/urls.py
+++ Aplicaciones/Modulo/urls.py
@@ -7,12 +7,12 @@ from django.contrib.auth.views import logout_then_login
 app_name='modulo'
 urlpatterns = [
     path('', views.LogInUser, name='login'),
-    """ path('', login_required(booksPage), name='BooksPage'),
-    path('BookDetail/<id>',bookDetail, name='BookDetail'),
-    path('BookRequest/<id>', bookRequest, name='BookRequest'),
-    path('AdminBooksPage/', adminView, name='AdminBooksPage'),
-    path('AdminBookDetail/<id>/', adminBookDetail, name='AdminBookDetail'),
-    path('AdminDeleteBook/<id>/',adminDeleteBook, name='AdminDeleteBook'),
-    path('AdminAddBook/', adminAddBook, name='AdminAddBook'),
-    path('AdminRequestList/', adminRequestList, name='AdminRequestList'), """
+    # path('', login_required(booksPage), name='BooksPage'),
+    # path('BookDetail/<id>',bookDetail, name='BookDetail'),
+    # path('BookRequest/<id>', bookRequest, name='BookRequest'),
+    # path('AdminBooksPage/', adminView, name='AdminBooksPage'),
+    # path('AdminBookDetail/<id>/', adminBookDetail, name='AdminBookDetail'),
+    # path('AdminDeleteBook/<id>/',adminDeleteBook, name='AdminDeleteBook'),
+    # path('AdminAddBook/', adminAddBook, name='AdminAddBook'),
+    # path('AdminRequestList/', adminRequestList, name='AdminRequestList'),
 ] 
