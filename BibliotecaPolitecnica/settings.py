225d5a3b9cf1a34303120249d7119307f6892be5
Guidzero <guidopr27@hotmail.com>
2023-07-09 17:27:06 -0600

    Settings

--- BibliotecaPolitecnica/settings.py
+++ BibliotecaPolitecnica/settings.py
@@ -147,8 +147,8 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = '/media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'BibliotecaPolitecnica/static'),)
