b21cdb4a953d5c175e7ec6ecef594ff0437a0d3e
Guidzero <guidopr27@hotmail.com>
2023-07-09 17:05:27 -0600

    fixing images

--- BibliotecaPolitecnica/settings.py
+++ BibliotecaPolitecnica/settings.py
@@ -142,12 +142,13 @@ if not DEBUG:
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'BibliotecaPolitecnica/static'),)
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'BibliotecaPolitecnica/bookImages')
 MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
