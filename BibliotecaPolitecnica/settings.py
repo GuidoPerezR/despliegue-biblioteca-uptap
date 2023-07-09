9ea02f3116ad105b99f38223dcafcfb6bb7691e2
Guidzero <guidopr27@hotmail.com>
2023-07-09 16:47:04 -0600

    Creating new folder

--- BibliotecaPolitecnica/settings.py
+++ BibliotecaPolitecnica/settings.py
@@ -149,5 +149,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'BibliotecaPolitecnica/static'),)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'BibliotecaPolitecnica/bookImages')
 MEDIA_URL = '/media/'
